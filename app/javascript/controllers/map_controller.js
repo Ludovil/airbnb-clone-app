494343c691bf83f741db8a5bcd08759869750a06
Ludovic Saule <ludovic_saule@hotmail.fr>
2024-12-04 21:03:56 +0100

    mapbox implemented

---
+++ app/javascript/controllers/map_controller.js
@@ -0,0 +1,54 @@
+import { Controller } from "@hotwired/stimulus";
+import mapboxgl from "mapbox-gl";
+
+// Connects to data-controller="map"
+export default class extends Controller {
+  static values = {
+    apiKey: String,
+    markers: Array,
+  };
+
+  connect() {
+    mapboxgl.accessToken = this.apiKeyValue;
+
+    // Initialisation de la carte
+    this.map = new mapboxgl.Map({
+      container: this.element,
+      style: "mapbox://styles/mapbox/streets-v10",
+      center: [2.3522, 48.8566], // Coordonnées de Paris par défaut
+      zoom: 11,
+    });
+
+    this.#addMarkersToMap(); // Ajouter les marqueurs sur la carte
+    this.#fitMapToMarkers(); // Ajuster la vue de la carte selon les marqueurs
+  }
+
+  #addMarkersToMap() {
+    this.markersValue.forEach((marker) => {
+      new mapboxgl.Marker().setLngLat([marker.lng, marker.lat]).addTo(this.map);
+    });
+  }
+
+  #fitMapToMarkers() {
+    if (this.markersValue.length === 0) {
+      console.warn("Aucun marqueur disponible. Centrage par défaut.");
+      this.map.setCenter([2.3522, 48.8566]); // Coordonnées de Paris par défaut
+      this.map.setZoom(10);
+      return;
+    }
+
+    const bounds = new mapboxgl.LngLatBounds();
+    this.markersValue.forEach((marker) =>
+      bounds.extend([marker.lng, marker.lat])
+    );
+
+    if (this.markersValue.length === 1) {
+      // Cas d'un seul marqueur : centrer et définir un zoom manuel
+      this.map.setCenter([this.markersValue[0].lng, this.markersValue[0].lat]);
+      this.map.setZoom(14); // Ajuste le zoom selon tes besoins
+    } else {
+      // Cas de plusieurs marqueurs : ajuste les limites automatiquement
+      this.map.fitBounds(bounds, { padding: 50, maxZoom: 15, duration: 0 });
+    }
+  }
+}
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_12_04_110731) do
+ActiveRecord::Schema[7.1].define(version: 2024_12_04_175113) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -52,6 +52,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_12_04_110731) do
     t.string "title"
     t.text "description"
     t.decimal "price_per_night"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_apartments_on_user_id"
   end
 
