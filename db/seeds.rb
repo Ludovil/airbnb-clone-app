ea0e4466a73fb923d50866c21d9178ca1da22385
Tracy Kabuya <tracykabuya@gmail.com>
2024-12-04 14:35:03 +0100

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,8 +2,18 @@
 # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require "faker"
+
+7.times do
+  user = User.create!(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.email,
+    password: Faker::Internet.email
+  )
+
+  user.apartments.create!( # Create apartment associated with user
+    address: Faker::Address.street_address,
+    availability: Faker::Boolean.boolean
+  )
+end
